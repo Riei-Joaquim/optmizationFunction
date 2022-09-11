3681c8fb33d0eda9bc3fa736f63fef25d11b5800
Matheus Albuquerque <mvca@cin.ufpe.br>
2022-09-11 20:05:27 -0300

    fixed function equation

--- Functions/AckleyFunct.py
+++ Functions/AckleyFunct.py
@@ -10,11 +10,11 @@ class Ackley:
     def eval(self, xList):
         sumP1 = 0
         for xi in xList:
-            sumP1 += xi**2
+            sumP1 += np.power(xi, 2)
 
         sumP2 = 0
         for xi in xList:
             sumP2 += np.cos(self._c*xi)
         
         return -self._a*np.exp(- self._b*np.sqrt((1/self._d)*sumP1)) \
-                    - np.sqrt((1/self._d)*sumP2) + self._a + np.exp(1)
+                    - np.exp((1/self._d)*sumP2) + self._a + np.exp(1)
