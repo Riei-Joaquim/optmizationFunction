619950936a0dcaeec9e03eff486792f59cdbeb2a
Matheus Albuquerque <mvca@cin.ufpe.br>
2022-09-11 20:06:11 -0300

    updated execution

--- execution.py
+++ execution.py
@@ -1,12 +1,13 @@
-from Algorithms import EvolutionStrategy
-from Functions import AckleyFunct
-from Individual import Individuo
+from Algorithms.EvolutionStrategy import EvolutionStrategy
 
 def main():
 
-    evolutionStrategy = EvolutionStrategy(-15, 15, 100, 2)
-    evolutionStrategy.initPopulation()
+    evolutionStrategy = EvolutionStrategy(-15, 15, 30, 2)
+    evolutionStrategy.fit(200000)
+    print(evolutionStrategy.bestInd.X)
+    print(evolutionStrategy.bestInd.sigma)
+    print(evolutionStrategy.bestInd.fitness)
 
     
-
-
+if __name__ == "__main__":
+    main()
