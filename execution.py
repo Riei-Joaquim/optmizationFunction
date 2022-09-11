249ab65f36e0b66bff7d1211d74b8c4a1c858476
tsa3 <tsa3@cin.ufpe.br>
2022-09-11 17:10:04 -0300

    wip: create execution.py

---
+++ execution.py
@@ -0,0 +1,12 @@
+from Algorithms import EvolutionStrategy
+from Functions import AckleyFunct
+from Individual import Individuo
+
+def main():
+
+    evolutionStrategy = EvolutionStrategy(-15, 15, 100, 2)
+    evolutionStrategy.initPopulation()
+
+    
+
+
